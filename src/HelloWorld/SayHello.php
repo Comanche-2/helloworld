0e4c7bc6a6197366c9b31360031a3a8ca628fa65
Comanche-2 <killerf2006@gmail.com>
2016-01-18 11:51:32 +0530

    Modified

--- src/HelloWorld/SayHello.php
+++ src/HelloWorld/SayHello.php
@@ -2,10 +2,14 @@
 
 namespace HelloWorld;
 
-class SayHello {
+class SayHello implements HelloInterface{
     
     public static function hello(){
         return 'Hello World !, Composer';
+    }
+    
+    public function hello_mate(){
+        return "Interface";
     }
     
 }
